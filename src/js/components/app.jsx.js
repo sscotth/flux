86b7101db6a61090a132d3c44749d0a7978fbc6a
Scott <scott@sscotth.io>
2015-02-19 14:48:36 -0600

    initial structure and gulp setup

---
+++ src/js/components/app.jsx.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react'),
+
+    App   = React.createClass({
+
+  render: function render() {
+    return (
+      <h1>My First Component</h1>
+    );
+  }
+
+});
+
+module.exports = App;
---
+++ src/js/main.jsx.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var React = require('react'),
+
+    App   = require('./components/app.jsx');
+
+React.render(<App />, document.querySelector('.container'));
