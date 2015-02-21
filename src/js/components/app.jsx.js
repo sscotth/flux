323e175a26106301bcd78d9cbb75c99ba81a24f0
Scott <scott@sscotth.io>
2015-02-20 23:46:42 -0600

    use Header component

--- src/js/components/app.jsx.js
+++ src/js/components/app.jsx.js
@@ -1,18 +1,16 @@
 'use strict';
 
 var React = require('react'),
-    Catalog = require('../components/catalog.jsx'),
-    Cart = require('../components/cart.jsx'),
 
-    App   = React.createClass({
+    Header = require('./header.jsx'),
+
+    App;
+
+App = React.createClass({
   render: function render() {
     return (
       <div>
-        <h1>My Store</h1>
-        <hr />
-        <Catalog />
-        <hr />
-        <Cart />
+        <Header />
       </div>
     );
   }
